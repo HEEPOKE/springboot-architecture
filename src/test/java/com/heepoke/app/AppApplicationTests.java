796b5ee09992cef1ab4e2751bf0f6ea0790b4fcf
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-07-26 14:14:06 +0700

    :money_with_wings: first

---
+++ src/test/java/com/heepoke/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heepoke.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
