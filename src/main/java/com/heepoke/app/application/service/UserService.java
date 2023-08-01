46615fe3dcf28354d00639ad342717fa28beaf03
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-01 14:04:19 +0700

    :money_with_wings: fix service && controller

--- src/main/java/com/heepoke/app/application/service/UserService.java
+++ src/main/java/com/heepoke/app/application/service/UserService.java
@@ -1,5 +1,55 @@
 package com.heepoke.app.application.service;
 
+import org.springframework.stereotype.Service;
+
+import com.heepoke.app.domain.models.User;
+import com.heepoke.app.domain.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
-    
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        user.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        user.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User updateUser(Long userId, User updatedUser) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setUsername(updatedUser.getUsername());
+            user.setEmail(updatedUser.getEmail());
+            user.setTel(updatedUser.getTel());
+            user.setRole(updatedUser.getRole());
+            user.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+            return userRepository.save(user);
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
--- src/main/java/com/heepoke/app/infrastructure/adapter/controllers/UserController.java
+++ src/main/java/com/heepoke/app/infrastructure/adapter/controllers/UserController.java
@@ -1,5 +1,39 @@
 package com.heepoke.app.infrastructure.adapter.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.heepoke.app.application.service.UserService;
+import com.heepoke.app.domain.models.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/apis/users")
 public class UserController {
-    
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
+        return userService.updateUser(userId, updatedUser);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
 }
