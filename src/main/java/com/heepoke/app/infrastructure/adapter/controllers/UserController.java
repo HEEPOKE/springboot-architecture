51e99fa4e25aabafc24c7db7a888de2eb82f571c
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2023-08-02 17:04:48 +0700

    :money_with_wings: set service message

--- src/main/java/com/heepoke/app/infrastructure/adapter/controllers/UserController.java
+++ src/main/java/com/heepoke/app/infrastructure/adapter/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.heepoke.app.application.service.UserService;
 import com.heepoke.app.domain.models.User;
 import com.heepoke.app.infrastructure.adapter.messaging.ResponseMessage;
 import com.heepoke.app.infrastructure.adapter.messaging.StatusResponse;
+import com.heepoke.app.shared.constants.Message;
+import com.heepoke.app.shared.constants.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,35 +25,35 @@ public class UserController {
     @PostMapping("/create")
     public ResponseMessage<User> createUser(@RequestBody User user) {
         User savedUser = userService.saveUser(user);
-        StatusResponse status = new StatusResponse("200", "Success", "user", "");
+        StatusResponse status = new StatusResponse( Message.SUCCESS.getCode(), Message.SUCCESS.getMessage(), Service.USER_CREATE, "");
         return new ResponseMessage<>(status, savedUser);
     }
 
     @PutMapping("/update/{userId}")
     public ResponseMessage<User> updateUser(@PathVariable Long userId, @RequestBody User updatedUser) {
         User userUpdated = userService.updateUser(userId, updatedUser);
-        StatusResponse status = new StatusResponse("200", "Success", "user", "");
+        StatusResponse status = new StatusResponse(Message.SUCCESS.getCode(), Message.SUCCESS.getMessage(), Service.USER_UPDATE, "");
         return new ResponseMessage<>(status, userUpdated);
     }
 
     @GetMapping("/get")
     public ResponseMessage<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        StatusResponse status = new StatusResponse("200", "Success", "user", "");
+        StatusResponse status = new StatusResponse(Message.SUCCESS.getCode(), Message.SUCCESS.getMessage(), Service.USER_LIST, "");
         return new ResponseMessage<>(status, users);
     }
 
     @GetMapping("/find/{userId}")
     public ResponseMessage<Optional<User>> getUserById(@PathVariable Long userId) {
         Optional<User> user = userService.getUserById(userId);
-        StatusResponse status = new StatusResponse("200", "Success", "user", "");
+        StatusResponse status = new StatusResponse(Message.SUCCESS.getCode(), Message.SUCCESS.getMessage(), Service.USER_BY_ID, "");
         return new ResponseMessage<>(status, user);
     }
 
     @DeleteMapping("/delete/{userId}")
     public ResponseMessage<String> deleteUser(@PathVariable Long userId) {
         userService.deleteUser(userId);
-        StatusResponse status = new StatusResponse("200", "Success", "user", "");
+        StatusResponse status = new StatusResponse(Message.SUCCESS.getCode(), Message.SUCCESS.getMessage(), Service.USER_DELETE, "");
         return new ResponseMessage<>(status, "User deleted successfully");
     }
 }
---
+++ src/main/java/com/heepoke/app/shared/constants/Service.java
@@ -0,0 +1,12 @@
+package com.heepoke.app.shared.constants;
+
+public class Service {
+    private Service() {
+    }
+
+    public static final String USER_LIST = "List User";
+    public static final String USER_BY_ID = "Get User By ID";
+    public static final String USER_CREATE = "Create User";
+    public static final String USER_UPDATE = "Update User";
+    public static final String USER_DELETE = "Delete User";
+}
